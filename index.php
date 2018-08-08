3a12fd69b649c03e5ef754b5c41e57de781f7844
Sun Yudong <sunjerry019@gmail.com>
2018-08-08 08:59:03 +0800

    first draft of duty personnel today

--- index.php
+++ index.php
@@ -1,19 +1,20 @@
-<?php
-	
-	//include this file at the start of every page
-	//library.inc.php includes the all objects and the configuration
-	require("library.inc.php");
-	
-	//sets and returns duty personnel (as a User object) for 10 Sept 2018
-	//returns the duty personnel if duty personnel is already set for the day
-	$day = new Day("2018-09-10"); //all dates use YYYY-MM-DD
-	echo "The duty personnel for 10 Sept 2018 is:<br/>";
-	echo $day->setDutyPersonnel()->getDisplayName()."<br /><br />";
+<?php require("library.inc.php"); ?>
 
-	//users can be constructed by either passing its database ID or username
-	$user1 = new User(1);
-	$user2 = new User("charlie");
-	echo "The user with DB index 1 is:<br />".$user1->getDisplayName()."<br /><br />";
-	echo "The user with username \"charlie\" is:<br />".$user2->getDisplayName()."<br />";
-	
-?>
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Duty Planner</title>
+		<link href="//fonts.googleapis.com/css?family=Roboto|Source+Sans+Pro" rel="stylesheet">
+		<link rel="stylesheet" href="style.css">
+	</head>
+	<body class="start">
+		<h1>Duty Planner</h1>
+		<h3>The duty personnel for today is:</h3>
+		<div class="dutypersonnel">
+			<?php
+				$day = new Day(date("Y-m-d"));
+				echo $day->setDutyPersonnel()->getDisplayName();
+			?>
+		</div>
+	</body>
+</html>
