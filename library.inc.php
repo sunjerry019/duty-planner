590154ea6c23a35d6e14c4bfca4cd7ad41d89a1e
Sun Yudong <sunjerry019@gmail.com>
2018-08-08 08:58:31 +0800

    change weightage

--- library.inc.php
+++ library.inc.php
@@ -4,9 +4,9 @@
 	 * Main library to handle most if not all actions *
 	 *       Usage: require_once("library.php")       *
 	 **************************************************/
-	 
+
 	date_default_timezone_set('Asia/Singapore');
-	
+
 	require("User.inc.php");
 	require("Day.inc.php");
 	require("DutyPool.inc.php");
@@ -14,6 +14,6 @@
 	const DB_PATH = "sqlite:database.db";
 	const WEEKDAY_POINTS = 1;
 	const FRIDAY_POINTS = 1.5;
-	const WEEKEND_POINTS = 3;
+	const WEEKEND_POINTS = 2;
 
 ?>
