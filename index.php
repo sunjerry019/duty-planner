152fbe1cd769488288a6ea26b5f1b629015c4184
Sun Yudong <sunjerry019@gmail.com>
2018-09-01 20:09:09 +0800

    dumping working files

--- index.php
+++ index.php
@@ -1,20 +1,28 @@
 <?php require("library.inc.php"); ?>
+<?php $doc_root = "/duty-planner" ?>
 
 <!DOCTYPE HTML>
 <html>
 	<head>
 		<title>Duty Planner</title>
 		<link href="//fonts.googleapis.com/css?family=Roboto|Source+Sans+Pro" rel="stylesheet">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	    <!-- Compiled and minified CSS -->
+	    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/css/materialize.min.css">
+	    <!-- Compiled and minified JavaScript -->
+	    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/js/materialize.min.js"></script>
 		<link rel="stylesheet" href="style.css">
 	</head>
-	<body class="start">
-		<h1>Duty Planner</h1>
-		<h3>The duty personnel for today is:</h3>
-		<div class="dutypersonnel">
-			<?php
-				$day = new Day(date("Y-m-d"));
-				echo $day->setDutyPersonnel()->getDisplayName();
-			?>
-		</div>
+	<body>
+		<nav>
+			<div class="nav-wrapper teal darken-1">
+				<a href="<?php echo $doc_root ?>/" class="brand-logo">Duty Planner</a>
+				<ul id="nav-mobile" class="right hide-on-med-and-down">
+					<li><a href="sass.html">Sass</a></li>
+					<li><a href="badges.html">Components</a></li>
+					<li><a href="collapsible.html">JavaScript</a></li>
+				</ul>
+			</div>
+		</nav>
 	</body>
 </html>
